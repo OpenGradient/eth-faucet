82285a6b991d777e68537b78f5dd02707c942a29
kukac <adambalogh@users.noreply.github.com>
2025-01-23 11:46:28 -0500

    Update server.go

--- cmd/server.go
+++ cmd/server.go
@@ -18,7 +18,7 @@ import (
 
 var (
 	appVersion = "v1.2.0"
-	chainIDMap = map[string]int{"sepolia": 11155111, "holesky": 17000, "opengradient": 11820}
+	chainIDMap = map[string]int{"sepolia": 11155111, "holesky": 17000, "opengradient": 10744}
 
 	httpPortFlag = flag.Int("httpport", 8090, "Listener port to serve HTTP connection")
 	proxyCntFlag = flag.Int("proxycount", 0, "Count of reverse proxies in front of the server")
